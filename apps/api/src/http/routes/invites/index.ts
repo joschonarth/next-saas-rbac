54ea3ee91796ac431a88328e99c6fa92b9ead9b2
joschonarth <joschonarth@gmail.com>
2026-08-08 11:57:30 -0300

    feat(api): add reject invite route

--- apps/api/src/http/routes/invites/index.ts
+++ apps/api/src/http/routes/invites/index.ts
@@ -3,10 +3,12 @@ import { createInvite } from './create-invite'
 import { getInvite } from './get-invite'
 import { getInvites } from './get-invites'
 import { acceptInvite } from './accept-invite'
+import { rejectInvite } from './reject-invite'
 
 export async function inviteRoutes(app: FastifyInstance) {
   app.register(createInvite)
   app.register(getInvite)
   app.register(getInvites)
   app.register(acceptInvite)
+  app.register(rejectInvite)
 }
