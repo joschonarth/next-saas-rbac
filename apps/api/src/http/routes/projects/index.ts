69e04a56f52a64bf18bc3ad0022aeaf99e06e16d
joschonarth <joschonarth@gmail.com>
2026-08-08 11:15:34 -0300

    feat(api): add get project route

--- apps/api/src/http/routes/projects/index.ts
+++ apps/api/src/http/routes/projects/index.ts
@@ -1,8 +1,10 @@
 import type { FastifyInstance } from 'fastify'
 import { createProject } from './create-project'
 import { deleteProject } from './delete-project'
+import { getProject } from './get-project'
 
 export async function projectRoutes(app: FastifyInstance) {
   app.register(createProject)
   app.register(deleteProject)
+  app.register(getProject)
 }
