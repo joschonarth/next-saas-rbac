90daa0bd86c5f86fbf537a9d11d5384a1752c7ce
joschonarth <joschonarth@gmail.com>
2026-08-08 11:50:28 -0300

    feat(api): add get invite route

--- apps/api/src/http/routes/invites/index.ts
+++ apps/api/src/http/routes/invites/index.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import { createInvite } from './create-invite'
+import { getInvite } from './get-invite'
 
 export async function inviteRoutes(app: FastifyInstance) {
   app.register(createInvite)
+  app.register(getInvite)
 }
