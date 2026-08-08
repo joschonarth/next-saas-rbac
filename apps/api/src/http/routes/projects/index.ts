1aec376d1ef108e80f56e1c1f112431beff48d37
joschonarth <joschonarth@gmail.com>
2026-08-08 11:12:32 -0300

    feat(api): add delete project route

--- apps/api/src/http/routes/projects/index.ts
+++ apps/api/src/http/routes/projects/index.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 import { createProject } from './create-project'
+import { deleteProject } from './delete-project'
 
 export async function projectRoutes(app: FastifyInstance) {
   app.register(createProject)
+  app.register(deleteProject)
 }
