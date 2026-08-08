1fdf32083646f7a38d8a0e075a0d6a0038a55aff
joschonarth <joschonarth@gmail.com>
2026-08-07 21:45:43 -0300

    feat: enhance ability management with Zod schemas and new subjects

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -6,10 +6,24 @@ import {
 } from '@casl/ability'
 import { User } from './models/user'
 import { permissions } from './permissions'
-import { UserSubject } from './subjects/user'
-import { ProjectSubject } from './subjects/project'
+import { userSubject } from './subjects/user'
+import { projectSubject } from './subjects/project'
+import z from 'zod'
+import { organizationSubject } from './subjects/organization'
+import { inviteSubject } from './subjects/invite'
+import { billingSubject } from './subjects/billing'
 
-type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
+const appAbilitiesSchema = z.union([
+  projectSubject,
+  userSubject,
+  organizationSubject,
+  inviteSubject,
+  billingSubject,
+
+  z.tuple([z.literal('manage'), z.literal('all')]),
+])
+
+type AppAbilities = z.infer<typeof appAbilitiesSchema>
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
