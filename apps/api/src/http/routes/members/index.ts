98a8b1cad4573c630eea9eaf5bf03af80e8ed339
joschonarth <joschonarth@gmail.com>
2026-08-08 11:35:32 -0300

    feat(api): add update member route

--- apps/api/src/http/routes/members/index.ts
+++ apps/api/src/http/routes/members/index.ts
@@ -1,6 +1,8 @@
 import type { FastifyInstance } from 'fastify'
 import { getMembers } from './get-members'
+import { updateMember } from './update-member'
 
 export async function memberRoutes(app: FastifyInstance) {
   app.register(getMembers)
+  app.register(updateMember)
 }
