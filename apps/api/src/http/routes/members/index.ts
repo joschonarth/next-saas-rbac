cffed3c4c8cc067a652072fd893060aa4bd45a98
joschonarth <joschonarth@gmail.com>
2026-08-08 11:36:55 -0300

    feat(api): add remove member route

--- apps/api/src/http/routes/members/index.ts
+++ apps/api/src/http/routes/members/index.ts
@@ -1,8 +1,10 @@
 import type { FastifyInstance } from 'fastify'
 import { getMembers } from './get-members'
 import { updateMember } from './update-member'
+import { removeMember } from './remove-member'
 
 export async function memberRoutes(app: FastifyInstance) {
   app.register(getMembers)
   app.register(updateMember)
+  app.register(removeMember)
 }
