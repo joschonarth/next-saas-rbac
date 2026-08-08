92eebcaa4c58ccf196a85ce42b3d080f8331174f
joschonarth <joschonarth@gmail.com>
2026-08-08 10:55:22 -0300

    refactor(api): restructure server setup and modularize routes

---
+++ apps/api/src/http/app.ts
@@ -0,0 +1,56 @@
+import fastifyCors from '@fastify/cors'
+import fastify from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import fastifyJwt from '@fastify/jwt'
+import { errorHandler } from './error-handler'
+import { env } from '@saas/env'
+import { authRoutes } from './routes/auth'
+import { orgRoutes } from './routes/orgs'
+
+export const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.setErrorHandler(errorHandler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description: 'Full-stack SaaS with multi-tenant & RBAC.',
+      version: '1.0.0',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+})
+
+app.register(fastifyCors)
+
+app.register(authRoutes)
+app.register(orgRoutes)
---
+++ apps/api/src/http/routes/orgs/index.ts
@@ -0,0 +1,15 @@
+import type { FastifyInstance } from 'fastify'
+
+import { createOrganization } from './create-organization'
+import { getMembership } from './get-membership'
+import { getOrganization } from './get-organization'
+import { getOrganizations } from './get-organizations'
+import { updateOrganization } from './update-organization'
+
+export async function orgRoutes(app: FastifyInstance) {
+  app.register(createOrganization)
+  app.register(getMembership)
+  app.register(getOrganization)
+  app.register(getOrganizations)
+  app.register(updateOrganization)
+}
