7177c590b778e82c98bb6c0ae8ac996166f43828
joschonarth <joschonarth@gmail.com>
2026-08-08 20:29:27 -0300

    feat(web): add OrganizationSwitcher component to header

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image'
 import rocketseatIcon from '@/app/assets/rocketseat-icon.svg'
 
 import { ProfileButton } from './profile-button'
+import { Slash } from 'lucide-react'
+import { OrganizationSwitcher } from './organization-switcher'
 
 export function Header() {
   return (
@@ -13,6 +15,10 @@ export function Header() {
           className="size-6 dark:invert"
           alt="Rocketseat"
         />
+
+        <Slash className="text-border size-3 -rotate-[24deg]" />
+
+        <OrganizationSwitcher />
       </div>
 
       <div className="flex items-center gap-4">
