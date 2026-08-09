455451bf97049f4d891fa7d133da6a142bc4c9d3
joschonarth <joschonarth@gmail.com>
2026-08-09 08:36:34 -0300

    feat(web): implement CreateOrganization page with sheet component and form integration

---
+++ apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
@@ -0,0 +1,20 @@
+import { Sheet, SheetHeader, SheetTitle } from '@/components/ui/sheet'
+
+import { OrganizationForm } from '../../create-organization/organization-form'
+import { InterceptedSheetContent } from '@/components/intercepted-sheet-content'
+
+export default function CreateOrganization() {
+  return (
+    <Sheet defaultOpen>
+      <InterceptedSheetContent>
+        <SheetHeader>
+          <SheetTitle className="text-base">Create organization</SheetTitle>
+        </SheetHeader>
+
+        <div className="py-4">
+          <OrganizationForm />
+        </div>
+      </InterceptedSheetContent>
+    </Sheet>
+  )
+}
--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,16 +1,21 @@
 import { redirect } from 'next/navigation'
 
 import { isAuthenticated } from '@/auth/auth'
-import { Header } from '@/components/header'
 
 export default function AppLayout({
   children,
+  sheet,
 }: Readonly<{
   children: React.ReactNode
+  sheet: React.ReactNode
 }>) {
   if (!isAuthenticated()) {
     redirect('/auth/sign-in')
   }
 
-  return <>{children}</>
+  return (
+    <>
+      {children} {sheet}
+    </>
+  )
 }
