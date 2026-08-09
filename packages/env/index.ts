a9934d8ed997497036431ae06c97a97115506e94
joschonarth <joschonarth@gmail.com>
2026-08-08 21:41:47 -0300

    feat(web): add NEXT_PUBLIC_API_URL to environment variables and update api-client to use it

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -16,7 +16,9 @@ export const env = createEnv({
     GITHUB_OAUTH_CLIENT_REDIRECT_URI: z.url(),
   },
   client: {},
-  shared: {},
+  shared: {
+    NEXT_PUBLIC_API_URL: z.url(),
+  },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     SERVER_PORT: process.env.SERVER_PORT,
@@ -26,6 +28,7 @@ export const env = createEnv({
     GITHUB_OAUTH_CLIENT_SECRET: process.env.GITHUB_OAUTH_CLIENT_SECRET,
     GITHUB_OAUTH_CLIENT_REDIRECT_URI:
       process.env.GITHUB_OAUTH_CLIENT_REDIRECT_URI,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   emptyStringAsUndefined: true,
 })
