84f4efa028d8a617ffc5684cd97e61c48d29beb0
joschonarth <joschonarth@gmail.com>
2026-08-09 10:08:02 -0300

    feat(web): restructure organization management components and add settings page

--- apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
+++ apps/web/src/app/(app)/@sheet/(.)create-organization/page.tsx
@@ -1,6 +1,6 @@
 import { Sheet, SheetHeader, SheetTitle } from '@/components/ui/sheet'
 
-import { OrganizationForm } from '../../create-organization/organization-form'
+import { OrganizationForm } from '../../org/organization-form'
 import { InterceptedSheetContent } from '@/components/intercepted-sheet-content'
 
 export default function CreateOrganization() {
